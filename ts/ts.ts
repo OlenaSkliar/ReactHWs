fd2162ad3d88a59fd0da5ac87e3d39c67bb05066
OlenaSkliar <forwardwithtime@gmail.com>
2023-07-06 01:15:56 +0300

    ts new

--- ts/ts.ts
+++ ts/ts.ts
@@ -8,9 +8,10 @@ interface MyInterface {
 //3
 interface MyArray<T> {
     [N: number]: T;
-    reduce<U>(accumulator: U, value: T): U;
+    reduce(accum: T, item: T): T[];
 }
+
 //4
 type MyPartial<T> = {
-    [N in keyof T]: T[N]
+    [N in keyof T]?: T[N] extends object ? MyPartial<T[N]> : T[N]
 }
